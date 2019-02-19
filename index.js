3009a4b2ae25058993c58754fe8452dc632d9f55
Carlos Barrera <barrera9696@gmail.com>
2019-02-19 20:55:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ function takeANumber(line, name) {
 return (`Welcome, ${name}. You are number ${line.length} in line.`);
 } 
 function nowServing(katzDeli) {
-var i=0;
-while(i < katzDeli.length) {i++}
 if (katzDeli.length === 0) {
 return 'There is nobody waiting to be served!';
 } else {
